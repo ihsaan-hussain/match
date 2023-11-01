54cac5e11892f4c02a0d00a683516e9a04d8e96d
Ihsaan <ihsaan09@outlook.com>
2023-11-01 17:59:36 +0000

    added clear fields

--- match_app.py
+++ match_app.py
@@ -51,8 +51,14 @@ class App:
         self.yellow_score_entry = Entry(scorelabelframe,font=("Helvetica", 20, 'bold'), bd=5, width=5, justify=CENTER)
         self.yellow_score_entry.grid(row=0,column=3, padx=10, pady=10)
 
-        self.save_score = Button(scorelabelframe,text="Save score",bg="light grey",font=("Helvetica",20,'bold'),bd=5,width=20)
+        self.save_score = Button(scorelabelframe,text="Save score",bg="light grey",font=("Helvetica",20,'bold'),bd=5,width=20,command=self.savescore)
         self.save_score.grid(row=0,column=4,padx=10,pady=10)
+
+        self.current_file_label = Label(scorelabelframe, bg="light blue", font=("Helvetica", 15, 'bold'), text="Current file: ")
+        self.current_file_label.grid(row=0, column=5, padx=10, pady=10)
+
+        self.current_entry = Entry(scorelabelframe, bd=5, font=("Helvetica", 20, 'bold'))
+        self.current_entry.grid(row=0,column=6,padx=10,pady=10)
 
         self.selected = StringVar()
         self.team_pick = ttk.Combobox(players_frame, textvariable=self.selected)
@@ -79,11 +85,14 @@ class App:
         self.player_assists_entry = Entry(players_frame, bd=5, font=("Helvetica", 20, 'bold'), width=20, justify=CENTER)
         self.player_assists_entry.grid(row=3,column=1,padx=10,pady=10)
 
-        self.add_player = Button(players_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Add Player", width=25)
+        self.add_player = Button(players_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Add Player", width=25,command=self.addplayer)
         self.add_player.grid(row=4,column=0,padx=10,pady=10)
 
-        self.clear_fields = Button(players_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Clear Fields", width=25)
+        self.clear_fields = Button(players_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Clear Fields", width=25,command=self.clearplayers)
         self.clear_fields.grid(row=4,column=1,padx=10,pady=10)
+
+        self.filler_label = Label(player_frame, text="                                                                                                                                  ", bg="light blue", fg="light blue", font=("Helvetica", 15, 'bold'))
+        self.filler_label.pack(padx=10,pady=10)
 
         self.motm_label = Label(extrainfos_frame, bd=5, bg="light blue", font=("Helvetica",20,'bold'), text="MOTM: ")
         self.motm_label.grid(row=0,column=0,padx=10,pady=10)
@@ -109,14 +118,55 @@ class App:
         self.possesion2_entry = Entry(extrainfos_frame, bd=5, font=("Helvetica", 20, 'bold'), width=5, justify=CENTER)
         self.possesion2_entry.grid(row=3,column=1,padx=10,pady=10)
 
-        self.add_info = Button(extrainfos_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Add Extra Info", width=25)
+        self.add_info = Button(extrainfos_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Add Extra Info", width=25,command=self.addinfo)
         self.add_info.grid(row=4,column=0,padx=10,pady=10)
 
-        self.clear_fields = Button(extrainfos_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Clear Fields", width=25)
+        self.clear_fields = Button(extrainfos_frame, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Clear Fields", width=25,command=self.clearinfo)
         self.clear_fields.grid(row=4,column=1,padx=10,pady=10)
 
-        self.create_spreadsheet = Button(buttonlabelframe, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Create Spreadsheet", width=25)
-        self.create_spreadsheet.grid(row=0,column=0,padx=10,pady=10)
+        self.create_spreadsheet = Button(buttonlabelframe, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Create Spreadsheet", width=25,command=self.createspreadsheet)
+        self.create_spreadsheet.grid(row=0,column=0,padx=30,pady=10)
+
+        self.open_spreadsheet = Button(buttonlabelframe, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Open Spreadsheet", width=25,command=self.openspreadsheet)
+        self.open_spreadsheet.grid(row=0,column=1,padx=30,pady=10)
+
+        self.add_spreadsheet = Button(buttonlabelframe, bd=5, bg="light grey", font=("Helvetica",15,'bold'), text="Save to Spreadsheet", width=25,command=self.addspreadsheet)
+        self.add_spreadsheet.grid(row=0,column=2,padx=30,pady=10)
+
+        self.see_players = Button(buttonlabelframe, bd=5, bg="light grey", font=("Helvetica",15,'bold'),text="See added players", width=25,command=self.seeplayers)
+        self.see_players.grid(row=0, column=3, padx=30, pady=10)
+
+    def seeplayers(self):
+        pass
+
+    def addspreadsheet(self):
+        pass
+
+    def openspreadsheet(self):
+        pass
+
+    def createspreadsheet(self):
+        pass
+
+    def addinfo(self):
+        pass
+
+    def clearinfo(self):
+        self.motm_entry.delete(0,END)
+        self.topscorer_entry.delete(0,END)
+        self.possesion_entry.delete(0,END)
+        self.possesion2_entry.delete(0,END)
+
+    def clearplayers(self):
+        self.player_entry.delete(0,END)
+        self.player_goals_entry.delete(0,END)
+        self.player_assists_entry.delete(0,END)
+
+    def addplayer(self):
+        pass
+
+    def savescore(self):
+        pass 
 
 a = App(root)
 
